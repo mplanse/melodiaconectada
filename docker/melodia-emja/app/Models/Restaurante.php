d735815327a761031141f0112f42058ac502ed82
Marc Plans <mplanse2122@politecnics.barcelona>
2025-02-17 20:11:20 +0100

    modelos y controladores de la bd

---
+++ docker/melodia-emja/app/Models/Restaurante.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurante extends Model
+{
+    use HasFactory;
+    protected $table = 'restaurantes';
+    protected $primaryKey = 'idRestaurante';
+    public $timestamps = false;
+    protected $fillable = ['direccion'];
+    
+    public function eventos()
+    {
+        return $this->hasMany(Evento::class, 'restaurantes_idRestaurante', 'idRestaurante');
+    }
+}
