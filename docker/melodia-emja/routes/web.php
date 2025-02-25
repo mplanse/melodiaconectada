80e4936ae9d516df24b038bf1bd0e781ef1dbed3
Arnau <aventurac2122@politecnics.barcelona>
2025-02-25 18:42:39 +0100

    docker completo

--- docker/melodia-emja/routes/web.php
+++ docker/melodia-emja/routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\EventoController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.layout');
 });
 
 Route::resource('eventos', EventoController::class);
