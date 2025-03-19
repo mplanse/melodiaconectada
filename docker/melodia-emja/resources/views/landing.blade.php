d95090a8be585a894d41f69ac6d1a353aa7ab3d6
Eric Concejero Moreno <133874397+EricConcejero@users.noreply.github.com>
2025-03-19 20:24:37 +0100

    abanzando con la landing
    no esta terminado eesta feo aun no os asusteis :D

--- docker/melodia-emja/resources/views/landing.blade.php
+++ docker/melodia-emja/resources/views/landing.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <div class="contenedor">
-        <img src="{{ asset('images/logo.png') }}" alt="BeatBite Logo" class="logo">
+        <img src="{{ asset('img/logo.png') }}" alt="BeatBite Logo" class="logo">
         <h1>"BeatBite: Donde la música en vivo encuentra su escenario perfecto."</h1>
         <div class="botones">
             <a href="#" class="btn btn-iniciar">Iniciar sesión</a>
@@ -20,5 +20,26 @@
             &#9660;
         </div>
     </div>
-</body>
+
+    <div class='carrusel'>
+        <h1 class='h1_melodia'>🎶 MELODÍA CONECTADA: DA VIDA A TU <br> NEGOCIO CON MÚSICA EN DIRECTO 🎶</h1>
+
+        <h4>Músicos y restaurantes conectados en una sola plataforma.</h4>
+
+
+
+        <div class="carousel">
+        <button class="carousel-button left">&#9664;</button>
+        <img src="{{ asset('img/imagen1.jpg') }}">
+        <img src="{{ asset('img/imagen2.jpg') }}">
+        <img src="{{ asset('img/imagen3.jpg') }}">
+        <img src="{{ asset('img/imagen4.jpg') }}">
+        <img src="{{ asset('img/imagen5.jpg') }}">
+        <button class="carousel-button right">&#9654;</button>
+    </div>
+</div>
+
+    <script src="{{ asset('js/carousel.js') }}"></script>
+        
+</body> 
 </html>
