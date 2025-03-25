996398cc210e17449d10395edaf8c01cd15eb718
Marc Plans <mplanse2122@politecnics.barcelona>
2025-03-25 19:02:41 +0100

    evento individual backend

--- docker/melodia-emja/routes/api.php
+++ docker/melodia-emja/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MapController;
-use App\Http\Controllers\Api\EventoController;
+use App\Http\Controllers\EventoController;
 
 
 
