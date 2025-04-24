7bb9bbb9aac25d2f0641e6100f6c5ca4cb36ba1f
Arnau <aventurac2122@politecnics.barcelona>
2025-04-24 19:02:43 +0200

    docker

--- docker/melodia-emja/vite.config.js
+++ docker/melodia-emja/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
+import { build, defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-
         laravel({
             input: ['resources/css/app.css', 'resources/css/app.scss', 'resources/js/app.js', 'resources/js/bootstrap.js'],
             refresh: true,
