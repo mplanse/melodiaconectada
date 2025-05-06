689f2250ef598df08ab08ea3ef532e2886ec4bdc
Arnau <aventurac2122@politecnics.barcelona>
2025-05-06 17:29:06 +0200

    Update layout.blade.php

--- docker/melodia-emja/resources/views/layouts/layout.blade.php
+++ docker/melodia-emja/resources/views/layouts/layout.blade.php
@@ -9,8 +9,8 @@
     @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/js/bootstrap.js'])
 
     <!-- Aquí se incluirían otras bibliotecas si las necesitas, como Bootstrap -->
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
         body {
             background-color: #212529;
